53e7f6803f9c0eedac2491cc87ee5960515e6a62
João Francisco Fabbri Fonseca <joao.fonseca27@fatec.sp.gov.br>
2025-05-23 16:26:20 -0300

    Salvando curso

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida, foi MULTADO!')
+}
+
+console.log('Dirija sempre utilizando cinto de segurança')
