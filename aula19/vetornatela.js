920aea45be5701f583691d975e0a7e08385daaf2
João Francisco Fabbri Fonseca <joao.fonseca27@fatec.sp.gov.br>
2025-06-05 20:42:11 -0300

    Salvando

---
+++ aula19/vetornatela.js
@@ -0,0 +1,14 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+valores.sort()
+
+/* 
+for(var i=0; i < valores.length; i++){
+    
+    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
+    */
+
+for(let i in valores){
+    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
